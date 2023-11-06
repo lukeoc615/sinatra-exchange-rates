5ebe5885e80c3dc39843a4f51845c0bae2099e14
Luke O'Connell <147653898+lukeoc615@users.noreply.github.com>
2023-11-06 20:56:51 +0000

    added dynamic path segments

--- app.rb
+++ app.rb
@@ -24,3 +24,20 @@ get("/") do
   # render a view template where I show the symbols
   erb(:homepage)
 end
+
+get("/:from_currency") do
+  @original_currency = params.fetch("from_currency")
+
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  
+  # some more code to parse the URL and render a view template
+end
+
+get("/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+  
+  # some more code to parse the URL and render a view template
+end
