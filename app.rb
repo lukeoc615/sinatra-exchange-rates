ed15168eb04546236287db74e969ad6bbe8e6167
Luke O'Connell <147653898+lukeoc615@users.noreply.github.com>
2023-11-06 18:53:40 +0000

    created symbols

--- app.rb
+++ app.rb
@@ -1,26 +1,26 @@
 require "sinatra"
 require "sinatra/reloader"
 require "http"
-
+require "json"
 # define a route
 get("/") do
 
   # build the API url, including the API key in the query string
   api_url = "https://api.exchangerate.host/list?access_key=" + ENV.fetch("EXCHANGE_RATE_KEY")
-
+  
   # use HTTP.get to retrieve the API information
   raw_data = HTTP.get(api_url)
 
   # convert the raw request to a string
   raw_data_string = raw_data.to_s
-
+  
   # convert the string to JSON
-  require json
+  
   parsed_data = JSON.parse(raw_data_string)
 
   # get the symbols from the JSON
-  # @symbols = parsed_data ...
+  @symbols = parsed_data.fetch("currencies").keys
 
   # render a view template where I show the symbols
-  # erb(:homepage)
+  erb(:homepage)
 end
